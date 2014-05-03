8cc1804bec53793b3bf5a6669bb4121d1f42fc46
zeroxfire <zero_korn@hotmail.com>
2014-05-03 13:55:56 +0700

    add profile name by url

--- pumbaa/views/profile.py
+++ pumbaa/views/profile.py
@@ -3,8 +3,8 @@ from pumbaa import models
 
 @view_config(route_name='profile.index', renderer='/profile/index.mako')
 def index(request):
-    profile_name="Zero"
-    
-    return dict(
-                profile_name=profile_name
-                )
+	profile_id = request.matchdict['profile_id']
+	
+	return dict(
+				profile_id=profile_id
+				)
